cf1264c82d1161e5ab20aa395fea6af848f66d0e
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-10-04 22:47:30 +0100

    add extra row to table

--- src/pages/employment.js
+++ src/pages/employment.js
@@ -10,16 +10,13 @@ export default ({ data }) => {
     query: "",
   })
 
-  const allFeaturedImages = data.allFile.edges || []
-  const allPosts = data.allMarkdownRemark.edges || []
   const allJobs = data.allJobListingsCsv.edges || []
   const regex = /\/[blog].*\/|$/
-  const featuredImageMap = Utils.getImageMap(allFeaturedImages, regex)
 
   const handleChange = e => {
     const query = e.target.value
 
-    const filteredData = allPosts.filter(post => {
+    const filteredData = allJobs.filter(post => {
       // query will run on the following fields
       const { description, title, tags, author } = post.node.frontmatter
       // standardize query
@@ -40,7 +37,7 @@ export default ({ data }) => {
   }
 
   const { filteredData, query } = state
-  const filteredPosts = query !== "" ? filteredData : allPosts
+  // const filteredPosts = query !== "" ? filteredData : allPosts
 
   return (
     <PageLayout>
@@ -64,6 +61,7 @@ export default ({ data }) => {
             <th>Strength(s)</th>
             <th>Weakness(es)</th>
             <th>Rating</th>
+            <th>Category</th>
           </tr>
         </thead>
         {allJobs.map(( listValue, index ) => {
@@ -72,7 +70,7 @@ export default ({ data }) => {
               <td><a href={listValue.node.url}>{listValue.node.name}</a></td>
               <td>{listValue.node.summary}</td>
               <td>{listValue.node.strengths}</td>
-              <td>{listValue.node.weaknesses}</td>
+              <td>{listValue.node.weakeness}</td>
               <td>{listValue.node.rating}</td>
               <td>{listValue.node.category}</td>
             </tr>
@@ -87,22 +85,6 @@ export default ({ data }) => {
           </tr> */}
         {/* </tbody> */}
       </Table>
-      <Container
-        fluid
-        className="p-3 w-auto text-left d-flex flex-wrap justify-content-center"
-      >
-        {filteredPosts.map(({ node }) => (
-          <div key={node.id} className="p-3">
-            <BlogLink
-              to={node.fields.slug}
-              featuredImage={featuredImageMap[node.fields.slug]}
-              title={node.frontmatter.title}
-              subtitle={node.frontmatter.date}
-              excerpt={node.excerpt}
-            />
-          </div>
-        ))}
-      </Container>
     </PageLayout>
   )
 }
@@ -119,46 +101,6 @@ export const query = graphql`
           weakeness
           rating
           category
-        }
-      }
-    }
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/blog/" } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            description
-            tags
-            author
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-    allFile(
-      filter: {
-        extension: { eq: "jpg" }
-        relativePath: { regex: "/feature/" }
-        relativeDirectory: { regex: "/content/blog/" }
-      }
-    ) {
-      edges {
-        node {
-          childImageSharp {
-            fluid(maxWidth: 400) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-          relativePath
         }
       }
     }
