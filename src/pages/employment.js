4f3c3016fd403816a24ab88e384ce52810eaed61
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-10-07 15:44:42 +0100

    change employment layout

--- src/pages/employment.js
+++ src/pages/employment.js
@@ -2,7 +2,8 @@ import React, { useState } from "react"
 import { graphql } from "gatsby"
 import { PageLayout, PageTitle, BlogLink } from "../components"
 import { SEO, Utils } from "../utils"
-import { Container, Form, FormControl, Table, Row, Dropdown } from "react-bootstrap"
+import { Container, FormControl, Table, InputGroup, Button } from "react-bootstrap"
+
 
 export default ({ data }) => {
   const [state, setState] = useState({
@@ -10,21 +11,25 @@ export default ({ data }) => {
     query: "",
   })
 
-  const allJobs = data.allJobListingsCsv.edges || []
+  const allFeaturedImages = data.allFile.edges || []
+  const allPosts = data.allMarkdownRemark.edges || []
   const regex = /\/[blog].*\/|$/
+  const featuredImageMap = Utils.getImageMap(allFeaturedImages, regex)
 
   const handleChange = e => {
     const query = e.target.value
 
-    const filteredData = allJobs.filter(post => {
+    const filteredData = allPosts.filter(post => {
       // query will run on the following fields
-      const { name, url, summary, strengths, weakeness, rating, category } = post.node
+      const { description, title, tags, author } = post.node.frontmatter
       // standardize query
       const stdQuery = query.toLowerCase()
       return (
-        // post.node.excerpt.toLowerCase().includes(stdQuery) ||
-        (name && name.toLowerCase().includes(stdQuery)) ||
-        (category.toLowerCase().includes(stdQuery))
+        post.node.excerpt.toLowerCase().includes(stdQuery) ||
+        (description && description.toLowerCase().includes(stdQuery)) ||
+        title.toLowerCase().includes(stdQuery) ||
+        author.toLowerCase().includes(stdQuery) ||
+        (tags && tags.join("").toLowerCase().includes(stdQuery))
       )
     })
 
@@ -35,60 +40,44 @@ export default ({ data }) => {
   }
 
   const { filteredData, query } = state
-  // const filteredPosts = query !== "" ? filteredData : allPosts
-
+  const filteredPosts = query !== "" ? filteredData : allPosts
+  
   return (
     <PageLayout>
-      <SEO title="Employment" />
-      <PageTitle title="Employment Page" />
-      <Container className="px-5 mb-5 text-center">
-        <i>Here you can...</i> &nbsp;<br />
-        <i><b>NOTE TO NERC:</b> Ability to search using tag</i>
-        {/* <Row> */}
-        <Form>
+    <SEO title="Employment" />
+    <PageTitle title="Employment Page" />
+    <Container className="px-5 mb-5 text-center">
+      <i>Here you can...</i> &nbsp;<br />
+      <i><b>NOTE TO NERC:</b> Ability to search using tag</i>
+      <InputGroup className="mb-3 blog-filter">
           <FormControl
             className="bg-none search"
             type="text"
             placeholder="Search"
             onChange={handleChange}
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
           />
-        </Form>
-        {/* <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Dropdown Button
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown> */}
-        {/* </Row> */}
+          <Button variant="outline-secondary" id="button-addon1">
+            Find
+          </Button>
+        </InputGroup>
       </Container>
-      <Container>
-      <Table striped hover>
-        <thead>
-          <tr>
-            <th>Job</th>
-            <th>Summary</th>
-            <th>Type</th>
-            <th>Location</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        {allJobs.map(( listValue, index ) => {
-          return (
-            <tr key={index}>
-              <td><a href={listValue.node.url}>{listValue.node.name}</a></td>
-              <td>{listValue.node.summary}</td>
-              <td>{listValue.node.type}</td>
-              <td>{listValue.node.location}</td>
-              <td>{listValue.node.category}</td>
-            </tr>
-          );
-        })}
-      </Table>
+      <Container
+        fluid
+        className="p-3 w-auto text-left d-flex flex-wrap justify-content-center"
+      >
+        {filteredPosts.map(({ node }) => (
+          <div key={node.id} className="p-3">
+            <BlogLink
+              to={node.fields.slug}
+              featuredImage={featuredImageMap[node.fields.slug]}
+              title={node.frontmatter.title}
+              subtitle={node.frontmatter.date}
+              excerpt={node.excerpt}
+            />
+          </div>
+        ))}
       </Container>
     </PageLayout>
   )
@@ -96,17 +85,46 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allJobListingsCsv {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/blog/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      totalCount
       edges {
         node {
-          name
-          url
-          summary
-          type
-          location
-          category
+          id
+          frontmatter {
+            title
+            description
+            tags
+            author
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+    allFile(
+      filter: {
+        extension: { eq: "jpg" }
+        relativePath: { regex: "/feature/" }
+        relativeDirectory: { regex: "/content/blog/" }
+      }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 400) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+          relativePath
         }
       }
     }
   }
 `
+
